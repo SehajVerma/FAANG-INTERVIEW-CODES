f476655e74ecb7bd29d748078006469c03780b7e
Sehaj Verma <43957804+SehajVerma@users.noreply.github.com>
2020-06-09 15:17:25 +0530

    Update Travelling_Salesman_Problem.cpp

--- Travelling_Salesman_Problem.cpp
+++ Travelling_Salesman_Problem.cpp
@@ -1,3 +1,30 @@
+vaise iski bruteforce complexity O(n!) 
+kyuki sare solutions permutations hai ek he string ke
+ A-------B
+ -       -
+ -     - - 
+ -   -   -
+ - -     -
+ C-------D
+ solutions can be A-B-C-D-A
+                  B-C-D-A-D
+                  C-D-A-D-B
+                  D-A-B-C-D
+ so n! times one element can be rotated
+ 
+ But with this bitMask approach complexity is O(2^N * N)
+let there be 4 cities
+0000
+0001
+---
+---
+---
+1111
+
+ These are till 2^N-1  //2^4-1 i.e 1 to 15 
+ so for one city we have to search for other N-1 cities so complexity O(2^N * N)
+
+
 #include <bits/stdc++.h>
 using namespace std;
 #define n 4
