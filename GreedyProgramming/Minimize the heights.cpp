49e8b91ba0b87fb17eeb5812fdd6201af6711a4d
Sehaj Verma <43957804+SehajVerma@users.noreply.github.com>
2020-06-30 19:12:31 +0530

    Create Minimize the heights.cpp

---
+++ GreedyProgramming/Minimize the heights.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int k,n;
+	    cin>>k>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	    cin>>a[i];
+	    
+	    sort(a,a+n);
+	    int ans=a[n-1]-a[0];
+	    int small = a[0]+k;
+	    int big = a[n-1]-k;
+	    if(small>big)
+	    swap(small,big);
+	    for(int i=1;i<n-1;i++)
+	    {
+	        int subtract = a[i]-k;
+	        int add = a[i]+k;
+	        
+	        if(subtract >= small || add <= big)
+	        continue;
+	        
+	        if(big-subtract <= add-small)
+	        small = subtract;
+	        else
+	        big = add;
+	    }
+	    cout<<min(ans,big-small)<<endl;
+	}
+	return 0;
+}
